cd86473e2cf0418ec56a61116a6a6a4a7fcc97f8
gabi <botezatug65@gmail.com>
2023-10-07 20:52:10 +0300

    Tax Calculator

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        double price = 100;
+        double tax = 25.11482;
+        int quantity = 3;
+        double total = 0;
+        String message = "I want to buy 3 shirt!";
+        System.out.println(message);
+            System.out.println( total = price*quantity*tax);
+        }
+
+}
